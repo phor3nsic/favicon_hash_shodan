99af621ecec4820aba30079c106d385898f463f0
Walleson Moura <phor3nsic@MacBook-Air-de-Walleson.local>
2021-07-28 10:18:05 -0300

    update source

--- favicon.py
+++ favicon.py
@@ -1,17 +1,24 @@
 import mmh3
 import requests
+import codecs
 import sys
-import urllib3
-urllib3.disable_warnings()
 
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 if len(sys.argv) < 2:
-	print("Example: ./favicon.py http://server/favicon.ico")
+	print("[!] Error!")
+	print(f"[-] Use: python3 {sys.argv[0]} http://example.com/favicon.ico")
+	print("[i] Get all hosts with the same favicon!")
 	sys.exit()
 
-url = sys.argv[1]
-req = requests.get(url, verify=False)
-result = req.content.encode('base64')
-favicon= str(mmh3.hash(result))
-print("[!]http.favicon.hash:"+favicon)
-print("[*]View Results:\nhttps://www.shodan.io/search?query=http.favicon.hash%3A"+favicon)
+def main():
+	response = requests.get(sys.argv[1], verify=False)
+	favicon = codecs.encode(response.content,"base64")
+	hash_favicon = mmh3.hash(favicon)
+
+	print("[!] http.favicon.hash:"+str(hash_favicon))
+	print("[*] View Results:\n> https://www.shodan.io/search?query=http.favicon.hash%3A"+str(hash_favicon))
+
+if __name__ == '__main__':
+	main()
